492ea6c70d2cadc28cdeb01b26f0183f0b51472b
Junaid1240651 <86207985+Junaid1240651@users.noreply.github.com>
2023-05-15 00:33:03 +0530

    Update Cart.js

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -46,7 +46,6 @@ const Cart = () => {
   };
   const placeOrderHandler = () => {
     setLoading2(true);
-    navigate("/checkout");
     axios
       .post(
         "https://musicart-full-stack-project-backend.onrender.com/checkOutProduct",
